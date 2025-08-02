e6057846796b78190cada269bbe6a6e4b245b201
Oskar Linak <linakoskar@gmail.com>
2025-08-02 17:00:00 +0200

    interrupts added to ivt up to rcc interrupt

--- startup_stm32f446r.c
+++ startup_stm32f446r.c
@@ -46,7 +46,72 @@ void isr_hardfault(void)
     while(1);
 }
 
-#define IVT_ARRAY_SIZE (4)
+void isr_memmanage(void)
+{
+    while(1);
+}
+
+void isr_busfault(void)
+{
+    while(1);
+}
+
+void isr_usagefault(void)
+{
+    while(1);
+}
+
+void isr_svcall(void)
+{
+    while(1);
+}
+
+void isr_debugmonitor(void)
+{
+    while(1);
+}
+
+void isr_pendsv(void)
+{
+    while(1);
+}
+
+void isr_systick(void)
+{
+    while(1);
+}
+
+void isr_wwdg(void)
+{
+    while(1);
+}
+
+void isr_pvd(void)
+{
+    while(1);
+}
+
+void isr_tamp_stamp(void)
+{
+    while(1);
+}
+
+void isr_rtc_wkup(void)
+{
+    while(1);
+}
+
+void isr_flash(void)
+{
+    while(1);
+}
+
+void isr_rcc(void)
+{
+    while(1);
+}
+
+#define IVT_ARRAY_SIZE (21)
 typedef void (*isr_t)(void);
 __attribute__((used,section(".ivt")))
 static const isr_t ivt[IVT_ARRAY_SIZE] =
@@ -55,4 +120,19 @@ static const isr_t ivt[IVT_ARRAY_SIZE] =
     isr_reset,
     isr_nmi,
     isr_hardfault,
+    isr_memmanage,
+    isr_busfault,
+    isr_usagefault,
+    0,0,0,0,
+    isr_svcall,
+    isr_debugmonitor,
+    0,
+    isr_pendsv,
+    isr_systick,
+    isr_wwdg,
+    isr_pvd,
+    isr_tamp_stamp,
+    isr_rtc_wkup,
+    isr_flash,
+    isr_rcc,
 };
